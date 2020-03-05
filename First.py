577a012bcd08bbc016f7c3edb2a3430004bed855
Satheesh Pandian Jeganathan <Satheesh.PJeganathan@nationwide.co.uk>
2020-03-05 21:31:51 +0000

    Added First Python Program

---
+++ First.py
@@ -0,0 +1,16 @@
+#name = input("Enter your name")
+#print ("Welcome "+ name)
+#print("Good bye")
+
+#Level 0
+#i = 2**38
+#print ( i)
+
+#level 1
+data = input("Enter your name: ")
+print ("Welcome "+data)
+print (data)
+a = 2
+b = 4
+c = a**b
+print(c)
